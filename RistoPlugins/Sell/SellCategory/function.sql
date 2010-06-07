562a69c3b5846184c996b3db126a360711741ef8
DaNiMoTh <jjdanimoth@gmail.com>
2010-06-07 19:05:32 +0200

    Added report function to SellCategory
    Signed-off-by: DaNiMoTh <jjdanimoth@gmail.com>

---
+++ RistoPlugins/Sell/SellCategory/function.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION report_sellcategory_amount (p_sellcat VARCHAR,
+                            p_date_from DATE, p_date_to DATE)
+    RETURNS NUMERIC AS $$
+    DECLARE
+        lunch_amount NUMERIC;
+        dinner_amount NUMERIC;
+    BEGIN
+        SELECT * INTO lunch_amount FROM get_sellcategory_amount(p_sellcat,
+            'l_sales',p_date_from,p_date_to);
+        SELECT * INTO dinner_amount FROM get_sellcategory_amount(p_sellcat,
+            'd_sales',p_date_from,p_date_to);
+
+        RETURN lunch_amount + dinner_amount;
+    END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION get_sellcategory_amount (p_sellcat VARCHAR,
+                            p_table VARCHAR, p_date_from DATE, p_date_to DATE)
+    RETURNS NUMERIC AS $$
+    DECLARE
+        stmt CHARACTER VARYING(1000);
+        amount NUMERIC;
+        sales RECORD;
+    BEGIN
+        stmt := 'SELECT s.quantity_sold, s.sell_price FROM ' || p_table ||
+            ' AS s, good_for_sale AS g WHERE s.article=g.id ';
+        stmt := stmt || 'AND g.sell_category=''' || p_sellcat || '''';
+        IF (p_date_from IS NOT NULL) THEN
+            stmt := stmt || ' AND sales_date>=''' || p_date_from || '''';
+        END IF;
+        IF (p_date_to IS NOT NULL) THEN
+            stmt := stmt || ' AND sales_date<=''' || p_date_to || '''';
+        END IF;
+
+        amount := 0;
+        FOR sales IN EXECUTE stmt LOOP
+            amount := amount + (sales.quantity_sold * sales.sell_price);
+        END LOOP;
+
+        RETURN amount;
+    END;
+$$ LANGUAGE 'plpgsql';
